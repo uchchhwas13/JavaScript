8a9ea06b9b3b7f191778ece3be0a9540e4a2dd7b
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-19 10:33:57 +0600

    Adding subtract, multiply and divide methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,7 +18,32 @@ function createAndWriteOutput(operator, enteredNumber) {
     outputResult(currentResult, calculationDescription);
 }
 
+function substractNumbers() {
+    const enteredNumber = parseInt(userInput.value);
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', enteredNumber);
+}
+
+function multiplyNumbers() {
+    const enteredNumber = parseInt(userInput.value);
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', enteredNumber);
+}
+
+function divideNumbers() {
+    const enteredNumber = parseInt(userInput.value);
+    if (enteredNumber === 0) {
+        alert('Cannot divide by zero!');
+        return;
+    }
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', enteredNumber);
+}
+
 addBtn.addEventListener('click', addNumbers);
+subtractBtn.addEventListener('click', substractNumbers);
+multiplyBtn.addEventListener('click', multiplyNumbers);
+divideBtn.addEventListener('click', divideNumbers);
 
 
 
