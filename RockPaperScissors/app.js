e90df32faf193fc339012792ca99d298d365af43
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-20 15:21:38 +0600

    Adding an anonymous function

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,1 +1,7 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const startGame = function () {
+    console.log('Game started!');
+}
+
+startGameBtn.addEventListener('click',  startGame);
