6e9b721925e4420059783c67025b831a078c9ae5
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-20 16:21:02 +0600

    Adding getPlayerChoice method

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,7 +1,32 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
 const startGame = function () {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
     console.log('Game started!');
+    const playerSelection = getPlayerChoice();
+    console.log(`Player's choice: ${playerSelection}`);
+}
+
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, '').toUpperCase();
+
+    if (selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS) { 
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you.`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection
 }
 
 startGameBtn.addEventListener('click',  startGame);
