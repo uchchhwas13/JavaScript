7f8dcca708157fca1f997fa3838a17cd06cfbc72
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-20 16:23:39 +0600

    Adding getComputerChoice method

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -29,4 +29,15 @@ const getPlayerChoice = function () {
     return selection
 }
 
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
 startGameBtn.addEventListener('click',  startGame);
