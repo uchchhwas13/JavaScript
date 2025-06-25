eed92197932a4006c014e2cf41f5db9f280456b8
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-25 12:25:15 +0600

    Finding all project items from html

--- Practice-oop/assets/scripts/app.js
+++ Practice-oop/assets/scripts/app.js
@@ -1,7 +1,27 @@
 class Tooltip{}
 
-class ProjectItem{}
+class ProjectItem{
+    constructor(id) {
+        this.id = id;
+    }
+}
 
-class ProjectList{}
+class ProjectList{
+    projects = [];
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+        console.log(prjItems);
+        for (const prjItem of prjItems) {
+            this.projects.push(new ProjectItem(prjItem.id));
+        }
+    }
+}
 
-class App{}
+class App{
+    static init() {
+        const activeProjectList = new ProjectList('active');
+        const finishedProjectList = new ProjectList('finished');
+    }
+}
+
+App.init();
