8b1963f00074239b742da503b5d633a99897ddbb
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-25 14:26:00 +0600

    Fixing bugs while moving elements

--- Practice-oop/assets/scripts/app.js
+++ Practice-oop/assets/scripts/app.js
@@ -1,4 +1,10 @@
 class DOMHelper {
+    static clearEventListeners(element) {
+        const clonedElement = element.cloneNode(true);
+        element.replaceWith(clonedElement);
+        return clonedElement;
+    }
+
     static moveElement(elementId, newDestinationSelector) {
         const element = document.getElementById(elementId);
         const destinationElement = document.querySelector(newDestinationSelector);
@@ -18,15 +24,21 @@ class ProjectItem{
 
     connectSwitchButton() {
         const projectItemElement = document.getElementById(this.id);
-        const switchBtn = projectItemElement.querySelector('button:last-of-type');
+        let switchBtn = projectItemElement.querySelector('button:last-of-type');
+        switchBtn = DOMHelper.clearEventListeners(switchBtn);
         switchBtn.addEventListener(
             'click', 
             this.updateProjectListsHandler.bind(null, this.id)
-);
+        );
     }
 
     connectMoreInfoButton() {
         
+    }
+
+    update(updateProjectListsFn, type) {
+        this.updateProjectListsHandler = updateProjectListsFn;
+        this.connectSwitchButton();
     }
 }
 
@@ -49,6 +61,7 @@ class ProjectList{
         // Logic to add a new project
         this.projects.push(project);
         DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+        project.update(this.switchProject.bind(this), this.type);
     }   
 
     switchProject(projectId) {
