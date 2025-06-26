b6a255a5eeb96bd2ef5fdd4a96293299c43ea7d4
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-06-26 12:54:58 +0600

    Storing object type data

--- storage-01-starting-setup/app.js
+++ storage-01-starting-setup/app.js
@@ -2,14 +2,21 @@ const storeBtn = document.getElementById('store-btn');
 const retrieveBtn = document.getElementById('retrieve-btn');
 
 const userId = 'u123';
-
+const user = {
+    id: userId,
+    name: 'Max',
+    age: 30
+};
 
 
 storeBtn.addEventListener('click', () => {
     localStorage.setItem('userId', userId);
+    localStorage.setItem('user', JSON.stringify(user));
 });
 
 retrieveBtn.addEventListener('click', () => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    console.log(storedUser);
     localStorage.getItem('userId')
         ? alert(`User ID: ${localStorage.getItem('userId')}`)
         : alert('No user ID found!');
